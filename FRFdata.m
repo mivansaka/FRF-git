0a3455defc215ccf83251b9cf8c88ea46b777125
Jiatai Deng <jiatai.deng@postgrad.manchester.ac.uk>
2019-11-25 15:20:31 +0000

    plot two basic plots for 10HzHealthy

--- FRFdata.m
+++ FRFdata.m
@@ -11,4 +11,23 @@ time = x10HzHealthy{:,1};
 AI0 = x10HzHealthy{:,2}/24;
 AI1 = x10HzHealthy{:,3}/24;
 
+%plot the origin data plot
+figure;
+grid on;
+subplot(2,1,1);
+plot(time,AI1);
+axis([0 6e+7 -0.1 0.2]);
+    %label plot
+title('Time domain image');
+xlabel('Time(s)');
+ylabel('Voltage');
+%basic fast fourier transform
+FAI1 = fft(AI1);
+subplot(2,1,2);
+plot(time,abs(FAI1));
+axis([0 3e+7 0 600]);
+    %label plot
+title('Frequency domain image');
+xlabel('Frequency(Hz)');
+ylabel('Amplitude');
 

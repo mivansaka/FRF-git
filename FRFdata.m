5375be45fd860247fc8c6aebc3eb495abc17be4e
mivansaka <d635663347@gmail.com>
2019-11-25 14:35:20 +0000

    assignment to variables

--- FRFdata.m
+++ FRFdata.m
@@ -1,4 +1,7 @@
 %this m file is for process the FRF data
 %read the Raw data file to workspace
-load(RawData.mat);
-%remember to import data
+load RawData.mat
+%remember to import data to workspace
+
+%assignment to variables
+

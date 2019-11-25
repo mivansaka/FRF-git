2a01e58df23203798dfe931a2296045839d35974
Jiatai Deng <jiatai.deng@postgrad.manchester.ac.uk>
2019-11-25 14:49:18 +0000

    recovery gained value

--- FRFdata.m
+++ FRFdata.m
@@ -4,4 +4,11 @@ load RawData.mat
 %remember to import data to workspace
 
 %assignment to variables
+%time is the same frequence,only choose one
+%AI1 is the Accelerometer signal
+time = x10HzHealthy{:,1};
+%recovery gained value
+AI0 = x10HzHealthy{:,2}/24;
+AI1 = x10HzHealthy{:,3}/24;
+
 
